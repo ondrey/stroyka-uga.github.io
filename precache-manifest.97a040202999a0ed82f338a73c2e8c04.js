0ea6e5a65454966b7e9ed29d3b578c62d8963569
Ondrey <mag.ondrei@gmail.com>
2020-04-06 15:08:44 +0300

    Перенес отчет на карточку сотрудника

---
+++ precache-manifest.97a040202999a0ed82f338a73c2e8c04.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f30256cff44b32319a07",
+    "url": "/stroyka-uga.github.io/css/chunk-vendors.7b0b955d.css"
+  },
+  {
+    "revision": "534f019f7c26754b081632e973d3075d",
+    "url": "/stroyka-uga.github.io/index.html"
+  },
+  {
+    "revision": "3c2cf7f40b8f23e3dfdb",
+    "url": "/stroyka-uga.github.io/js/about.1fa71bc4.js"
+  },
+  {
+    "revision": "ba5fe50da10b445c2f49",
+    "url": "/stroyka-uga.github.io/js/app.d7b2e1f5.js"
+  },
+  {
+    "revision": "f30256cff44b32319a07",
+    "url": "/stroyka-uga.github.io/js/chunk-vendors.f3cb8c7a.js"
+  },
+  {
+    "revision": "29f2a3f60d1dc81697b7a59bb44fae33",
+    "url": "/stroyka-uga.github.io/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/stroyka-uga.github.io/robots.txt"
+  }
+]);
